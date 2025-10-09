041fb7aff7dba7bf62d1aba5741eab9defc96c15
theARCania <chanchparaa@gmail.com>
2025-10-09 22:17:01 +0530

    Comment DS

---
+++ code/comment.c
@@ -0,0 +1,8 @@
+#include "comment.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+Comment* createComment(char* username, char* content) {
+  
+}
---
+++ code/comment.h
@@ -0,0 +1,15 @@
+#ifndef COMMENT_H
+#define COMMENT_H
+#include "reply.h"
+
+struct Comment {
+  char* username;
+  char* content;
+  int replyCount;
+  Reply* repliesHead;
+  Reply* repliesEnd;
+  struct Comment* next;
+};
+typedef struct Comment Comment;
+
+#endif
